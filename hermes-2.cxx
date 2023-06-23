4a1cf650354fd1a097915baf406f398dfa59651c
David Bold <dave@ipp.mpg.de>
2023-06-23 15:41:04 +0200

    Fix target flux

--- hermes-2.cxx
+++ hermes-2.cxx
@@ -2904,13 +2904,9 @@ int Hermes::rhs(BoutReal t) {
 	  // Heat flux
 	  BoutReal q = (sheath_gamma_e - 1.5) * tesheath * nesheath * vesheath;
 	  // Multiply by cell area to get power
-	  BoutReal flux =
-	    q
-	    * (coord->J(x, y, z) + coord->J.ynext(bndry_par->dir)(x, y + bndry_par->dir, z))
-	    / (sqrt(coord->g_22(x, y, z))
-	       + sqrt(coord->g_22.ynext(bndry_par->dir)(x, y + bndry_par->dir, z)));
+          BoutReal flux = q * coord->J(x, y, z) / sqrt(coord->g_22(x, y, z));
 
-	  // Divide by volume of cell, and 2/3 to get pressure
+          // Divide by volume of cell, and 2/3 to get pressure
 	  BoutReal power =
 	    flux
 	    / (coord->dy(x, y, z) * coord->J(x, y, z));
