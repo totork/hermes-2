da8af35735c7982e19a827670e555c6af2f1c087
David Bold <dave@ipp.mpg.de>
2024-02-20 09:40:13 +0100

    Update mms.py

--- mms.py
+++ mms.py
@@ -18,92 +18,94 @@ failed = collections.defaultdict(list)
 failed2 = set()
 
 
+def collect(path, var, mesh=0):
+    # print(var, path, mesh)
+    return boutcollect(
+        var, path=path, prefix=f"BOUT.mesh_{mesh}", strict=True, info=False
+    )
+
+
+ana_default = {
+    "R": lambda R, Z: 1 / R,
+    "R²": lambda R, Z: R * 0 + 4,
+    "sin(R)": lambda R, Z: np.cos(R) / R - np.sin(R),
+    "sin(10*R)": lambda R, Z: 10 * np.cos(10 * R) / R - 1e2 * np.sin(10 * R),
+    "sin(100*R)": lambda R, Z: 100 * np.cos(100 * R) / R - 1e4 * np.sin(100 * R),
+    "sin(1000*R)": lambda R, Z: 1000 * np.cos(1000 * R) / R - 1e6 * np.sin(1000 * R),
+    "sin(Z)*sin(R)": lambda R, Z: np.sin(Z) * np.cos(R) / R - 2 * np.sin(Z) * np.sin(R),
+    "sin(Z)": lambda R, Z: -np.sin(Z),
+    "sin(Z*10)": lambda R, Z: -np.sin(Z * 10) * 100,
+    "sin(Z*100)": lambda R, Z: -np.sin(Z * 100) * 1e4,
+    "sin(Z*1000)": lambda R, Z: -np.sin(Z * 1000) * 1e6,
+    "Z": lambda R, Z: Z * 0,
+}
+ana = dict()
+ana["bracket(a, f)"] = {
+    "R, Z": lambda R, Z: -1 / R,
+    "R, R": lambda R, Z: 0 * R,
+    "Z, R": lambda R, Z: 0 * R,
+    "Z, Z": lambda R, Z: 0 * R,
+    "sin(R), sin(Z)": lambda R, Z: -1 / R * np.cos(R) * np.cos(Z),
+}
+for a in 10, 100, 1000:
+    ana["bracket(a, f)"][f"sin(R*{a}), sin(Z*{a})"] = (
+        lambda R, Z, a=a: -1 / R * np.cos(a * R) * np.cos(a * Z) * a * a
+    )
+ana["bracket(a, f, OLD)"] = ana["bracket(a, f)"]
+ana["FCI::Div_a_Grad_perp(a, f)"] = {
+    "R, Z": lambda R, Z: 0 * R,
+    "R, R": lambda R, Z: 0 * R + 2,
+    "Z, R": lambda R, Z: Z / R,
+    "Z, Z": lambda R, Z: 0 * Z + 1,
+    "sin(R), sin(Z)": lambda R, Z: -np.sin(R) * np.sin(Z),
+}
+for a in 10, 100, 1000:
+    ana["FCI::Div_a_Grad_perp(a, f)"][f"sin(R*{a}), sin(Z*{a})"] = (
+        lambda R, Z, a=a: -np.sin(a * R) * np.sin(a * Z) * a * a
+    )
+ana["FCI::dagp(f)"] = ana["FCI::Div_a_Grad_perp(a, f)"]
+ana["FCI::dagp_fv(f)"] = ana["FCI::Div_a_Grad_perp(a, f)"]
+
+
+def get_ana(method, func):
+    try:
+        dic = ana[method]
+    except KeyError:
+        dic = ana_default
+    try:
+        return dic[func]
+    except:
+        print(method, func)
+        raise
+
+
 def doit(path):
-    def collect(var, mesh=0, path=path):
-        # print(var, path, mesh)
-        return boutcollect(
-            var, path=path, prefix=f"BOUT.mesh_{mesh}", strict=True, info=False
-        )
+    if 0:
+        r = os.system(f"build-cont-opt/hermes-mms -d {path} -q -q -q")
+        if r:
+            os.system(f"build-cont-opt/hermes-mms -d {path}")
+            raise RuntimeError("bout++ failed")
 
-    r = 0  # os.system(f"build-cont-opt/hermes-mms -d {path} -q -q -q")
-    if r:
-        os.system(f"build-cont-opt/hermes-mms -d {path}")
-        raise RuntimeError("bout++ failed")
-    assert r == 0
-    ana_default = {
-        "R": lambda R, Z: 1 / R,
-        "R²": lambda R, Z: R * 0 + 4,
-        "sin(R)": lambda R, Z: np.cos(R) / R - np.sin(R),
-        "sin(10*R)": lambda R, Z: 10 * np.cos(10 * R) / R - 1e2 * np.sin(10 * R),
-        "sin(100*R)": lambda R, Z: 100 * np.cos(100 * R) / R - 1e4 * np.sin(100 * R),
-        "sin(1000*R)": lambda R, Z: 1000 * np.cos(1000 * R) / R
-        - 1e6 * np.sin(1000 * R),
-        "sin(Z)*sin(R)": lambda R, Z: np.sin(Z) * np.cos(R) / R
-        - 2 * np.sin(Z) * np.sin(R),
-        "sin(Z)": lambda R, Z: -np.sin(Z),
-        "sin(Z*10)": lambda R, Z: -np.sin(Z * 10) * 100,
-        "sin(Z*100)": lambda R, Z: -np.sin(Z * 100) * 1e4,
-        "sin(Z*1000)": lambda R, Z: -np.sin(Z * 1000) * 1e6,
-        "Z": lambda R, Z: Z * 0,
-    }
-    ana = dict()
-    ana["bracket(a, f)"] = {
-        "R, Z": lambda R, Z: -1 / R,
-        "R, R": lambda R, Z: 0 * R,
-        "Z, R": lambda R, Z: 0 * R,
-        "Z, Z": lambda R, Z: 0 * R,
-        "sin(R), sin(Z)": lambda R, Z: -1 / R * np.cos(R) * np.cos(Z),
-    }
-    for a in 10, 100, 1000:
-        ana["bracket(a, f)"][f"sin(R*{a}), sin(Z*{a})"] = (
-            lambda R, Z, a=a: -1 / R * np.cos(a * R) * np.cos(a * Z) * a * a
-        )
-    ana["bracket(a, f, OLD)"] = ana["bracket(a, f)"]
-    ana["FCI::Div_a_Grad_perp(a, f)"] = {
-        "R, Z": lambda R, Z: 0 * R,
-        "R, R": lambda R, Z: 0 * R + 2,
-        "Z, R": lambda R, Z: Z / R,
-        "Z, Z": lambda R, Z: 0 * Z + 1,
-        "sin(R), sin(Z)": lambda R, Z: -np.sin(R) * np.sin(Z),
-    }
-    for a in 10, 100, 1000:
-        ana["FCI::Div_a_Grad_perp(a, f)"][f"sin(R*{a}), sin(Z*{a})"] = (
-            lambda R, Z, a=a: -np.sin(a * R) * np.sin(a * Z) * a * a
-        )
-    ana["FCI::dagp(f)"] = ana["FCI::Div_a_Grad_perp(a, f)"]
-    ana["FCI::dagp_fv(f)"] = ana["FCI::Div_a_Grad_perp(a, f)"]
-
-    def get_ana(method, func):
-        try:
-            dic = ana[method]
-        except KeyError:
-            dic = ana_default
-        try:
-            return dic[func]
-        except:
-            print(method, func)
-            raise
-
-    s = slice(2, -2), 1, slice(None)
+    s = slice(2, -2), slice(None), slice(None)
 
     Rs = []
     m = 0
     while 1:
         try:
-            Rs += [collect("R", m)]
+            Rs += [collect(path, "R", m)]
             m += 1
         except OSError as e:
             # print(e)
             break
     mids = range(m)
-    Zs = [collect("Z", m) for m in mids]
+    Zs = [collect(path, "Z", m) for m in mids]
     assert len(Rs) == len(Zs)
     assert len(Rs) > 1
     toplot = []
     mmax = 0
     while 1:
         try:
-            collect(f"out_{mmax}")
+            collect(path, f"out_{mmax}")
         except ValueError as e:
             break
         mmax += 1
@@ -112,13 +114,26 @@ def doit(path):
         l2 = []
         lst = []
         for m in mids:
-            o = collect(f"out_{i}", m)
+            o = collect(path, f"out_{i}", m)
             attrs = o.attributes
             ops, inp = attrs["operator"], attrs["inp"]
             a = get_ana(ops, inp)(Rs[m], Zs[m])
             e = (o - a)[s]
+            if 1:
+                s2 = slice(None), 0, slice(None)
+                print([x.shape for x in [Rs[m][s2], Zs[m][s2], o[s2]]])
+                plt.pcolormesh(Rs[m][s2], Zs[m][s2], o[s2])
+                plt.colorbar()
+                plt.figure()
+                plt.pcolormesh(Rs[m][s2], Zs[m][s2], a[s2])
+                plt.colorbar()
+                plt.figure()
+                plt.pcolormesh(Rs[m][s2], Zs[m][s2], (o - a)[s2])
+                plt.colorbar()
+                plt.show()
+
             l2.append(np.sqrt(np.mean(e**2)))
-            lst.append(Rs[m].shape[0] - 4)
+            lst.append(Rs[m].shape[2])
         if not np.any(a):
             print(ops, inp)
             continue
@@ -133,13 +148,15 @@ def doit(path):
         if not np.isclose(
             ord[-1], get_convergence(attrs["operator"]), atol=0.25, rtol=0
         ):
-            print(i, ord, {k: v for k, v in attrs.items() if "_" not in k and v})
+            print("F", i, ord, {k: v for k, v in attrs.items() if "_" not in k and v})
 
             global success, failed, failed2
             failed[ops].append(inp)
             if ops not in expected_failures:
                 success = False
                 failed2.add(ops)
+        else:
+            print("S", i, ord, {k: v for k, v in attrs.items() if "_" not in k and v})
 
         toplot.append((attrs["inp"], attrs["operator"], lst, l2))
         label = f'{attrs["inp"]} {attrs["operator"]}'
@@ -157,27 +174,29 @@ def doit(path):
     for a, b, c, d in toplot:
         toplot2[a].append((b, c, d))
         # toplot2[b].append((a, c, d))
-    for k, vs in toplot2.items():
-        plt.figure()
-        for ab, c, d in vs:
-            plt.plot(c, d, "x-", label=ab)
-        plt.title(k)
-        plt.legend()
-        plt.gca().set_yscale("log")
-        plt.gca().set_xscale("log")
-    plt.show()
+    if 0:
+        for k, vs in toplot2.items():
+            plt.figure()
+            for ab, c, d in vs:
+                plt.plot(c, d, "x-", label=ab)
+            plt.title(k)
+            plt.legend()
+            plt.gca().set_yscale("log")
+            plt.gca().set_xscale("log")
+        plt.show()
 
 
-if sys.argv[1:]:
-    args = sys.argv[1:]
-else:
-    args = glob.iglob("mms*/")
-for p in args:
-    # print(p)
-    doit(p)
+if __name__ == "__main__":
+    if sys.argv[1:]:
+        args = sys.argv[1:]
+    else:
+        args = glob.iglob("mms*/")
+    for p in args:
+        # print(p)
+        doit(p)
 
-if failed:
-    print(failed)
-if failed2:
-    print(failed2)
-sys.exit(0 if success else 1)
+    if failed:
+        print(failed)
+    if failed2:
+        print(failed2)
+    sys.exit(0 if success else 1)
