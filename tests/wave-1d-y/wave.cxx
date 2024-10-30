98d32197478b5ce9d9245b93074ab3d9c971a5ec
totork <ttork96@gmail.com>
2024-10-30 15:33:36 +0100

    More work on MMS test, problems with parallel boundaries

--- tests/wave-1d-y/wave.cxx
+++ tests/wave-1d-y/wave.cxx
@@ -7,7 +7,8 @@
 
 class Wave1D : public PhysicsModel {
 private:
-  Field3D f, g,f_solution,g_solution; // Evolving variables
+  Field3D f, g; // Evolving variables
+  Field3D f_solution, g_solution, f_source, g_source;
 
 protected:
   int init(bool restarting) override {
@@ -16,52 +17,59 @@ protected:
     auto& optg = opt["g"];
     f_solution = optf["solution"].withDefault(Field3D{0.0});
     g_solution = optg["solution"].withDefault(Field3D{0.0});
-    
+    f_source = optf["source"].withDefault(Field3D{0.0});
+    g_source = optg["source"].withDefault(Field3D{0.0});
+   
+    SAVE_REPEAT(f_solution, g_solution, f_source, g_source);
     // Tell BOUT++ to solve f and g
     solver->add(f, "f");
     solver->add(g, "g");
-
+    
     return 0;
   }
 
   int rhs(BoutReal UNUSED(t)) override {
+    auto& opt = Options::root();
+    auto& optf = opt["f"];
+    auto& optg = opt["g"];
+    f_solution = optf["solution"].withDefault(Field3D{0.0});
+    g_solution = optg["solution"].withDefault(Field3D{0.0});
+    f_source = optf["source"].withDefault(Field3D{0.0});
+    g_source = optg["source"].withDefault(Field3D{0.0});
+    
     f.applyBoundary();
     g.applyBoundary();
     mesh->communicate(f, g); // Communicate guard cells
 
+    
+    
     //set parallel Boundary conditions
-
     /*
-    for (const auto &bndry_par :
-	   mesh->getBoundariesPar(BoundaryParType::all)) {
-      for (bndry_par->first(); !bndry_par->isDone(); bndry_par->next()){
-	int x = bndry_par->ind().x();
-        int y = bndry_par->ind().y();
-        int z = bndry_par->ind().z();
-
-	f.ynext(bndry_par->dir)(x,y+bndry_par->dir,z) = f_solution(x, y, z);
-	g.ynext(bndry_par->dir)(x,y+bndry_par->dir,z) = g_solution(x, y, z);
-      }
-    }
-    */
-
     for (const auto& ind : f.getRegion("RGN_NOBNDRY")){
       if ((mesh->lastY()) && (ind.y() == mesh->yend)){
 	f(ind.x(),ind.y(),ind.z()) = f_solution(ind.x(),ind.y(),ind.z());
 	g(ind.x(),ind.y(),ind.z()) = g_solution(ind.x(),ind.y(),ind.z());
+
+	f(ind.x(),ind.y()-1,ind.z()) = f_solution(ind.x(),ind.y()-1,ind.z());
+        g(ind.x(),ind.y()-1,ind.z()) = g_solution(ind.x(),ind.y()-1,ind.z());
       }
       if ((mesh->firstY()) && (ind.y() == mesh->ystart)){
 	f(ind.x(),ind.y(),ind.z()) = f_solution(ind.x(),ind.y(),ind.z());
         g(ind.x(),ind.y(),ind.z()) = g_solution(ind.x(),ind.y(),ind.z());
+
+	f(ind.x(),ind.y()+1,ind.z()) = f_solution(ind.x(),ind.y()+1,ind.z());
+        g(ind.x(),ind.y()+1,ind.z()) = g_solution(ind.x(),ind.y()+1,ind.z());
       }
     }
+    */
+    g.applyParallelBoundary();
+    f.applyParallelBoundary();
     
 
     //Time evolution
-    ddt(f) = Div_par(g);
-    ddt(g) = Div_par(f);
-    //ddt(f) = 0.0;
-    //ddt(g) = 0.0;
+    ddt(f) = Grad_par(g) + f_source;
+    ddt(g) = Grad_par(f) + g_source;
+    
     return 0;
   }
 };
