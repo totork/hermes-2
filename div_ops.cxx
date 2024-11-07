753f950a47dbcdb085d13f2d3d2a389ca3717c89
totork <ttork96@gmail.com>
2024-11-07 15:04:51 +0100

    SpecificInd: Replace zpm with offset

--- div_ops.cxx
+++ div_ops.cxx
@@ -1280,7 +1280,8 @@ Field3D CustomStencil::apply(const Field3D &a, const std::string &region) {
 BoutReal CustomStencil::apply(const Field3D &a, const Ind3D &i) {
   BoutReal result = 0;
   for (size_t c = 0; c < coefs.size(); ++c) {
-    result += coefs[c][i] * a[i.xp(xoffset[c]).zpm(zoffset[c])];
+    //result += coefs[c][i] * a[i.xp(xoffset[c]).zpm(zoffset[c])]; OLD ONE, NOT WORKING WITH NEW BOUT VERSION
+    result += coefs[c][i] * a[i.offset(xoffset[c], 0, zoffset[c])];
   }
   return result;
 }
