b9b876b939720f5c9296f4bec54961cf7ae512b4
totork <ttork96@gmail.com>
2025-01-15 21:42:21 +0100

    Added test for distorted grids in circular geometry

---
+++ MMS/distorted_grid/distorted.m
@@ -0,0 +1,68 @@
+(* ::Package:: *)
+
+BeginPackage["axial`"]
+
+(* To run the package, execute the following line in a notebook
+<<axial.m
+ *)
+
+(* 
+"Provides MMS solution and source for the anisotropic diffusion model in circular geometry
+- Only Dirichlet boundary conditions in the perpendicular direction is considered
+- Parallel boundaries (Penalisation) are not taken into account 
+- MMS solution is firstly is prescribed in cylindrical coordinates 
+(r,p,z) and afterwards a transformed to Cartesian coordinates (x,y,phi) used in GRILLIX 
+r = sqrt(x^2+y^2);  p = arctan(y/x);  z = phi, t = time
+- Paramters of model are: chi_par, chi_perp, safety factor q, and limiting flux surfaces rmin and rmax"
+*)
+
+
+Print["Computing MMS Terms"];
+
+(*
+define x,y and parallel derivatives in terms of r,p,z derivative \
+and normalised radial coordinate rn
+*)
+absb[x_] = Sqrt[1 + x^2/q[x]^2];
+pgrad[f_, x_, z_, y_, t_] = (D[f[x,z,y,t],y] + 1/q[x]*D[f[x,z,y,t],z])/absb[x];
+ddx[f_, r_, p_, z_, t_] = D[f[r, p, z, t], r];
+ddy[f_, r_, p_, z_, t_] = D[f[r, p, z, t], r]*Sin[p] + D[f[r, p, z, t], p]*Cos[p]/r;
+d2dx2[f_, r_, p_, z_, t_] = D[ddx[f, r, p, z, t], r];
+d2dy2[f_, r_, p_, z_, t_] = D[ddy[f, r, p, z, t], r]*Sin[p] +  D[ddy[f, r, p, z, t], p]*Cos[p]/r;
+
+
+LaplacePerpMmsSol[f_,r_, p_, z_, t_] =  d2dx2[f, r, p, z, t] + d2dy2[f, r, p, z, t];
+
+LaplacePerp[f_,r_,p_,z_,t_] = D[f[r,p,z,t],{r,2}] + D[f[r,p,z,t],r]/r + 1.0/(r*r)*D[f[r,p,z,t],{p,2}];
+
+d2dpar2[f_, x_, z_, y_, t_] = Dpar * ((D[D[f[x, z, y, t], y], y] + 2/q[x]*D[D[f[x, z, y, t], y], z] + 
+     1/q[x]^2*D[D[f[x, z, y, t], z], z])/absb[x]^2);
+  
+xn[x_] = (x-xmin)/(xmax-xmin);  
+    (*
+    d2dpar2[f_, x_, z_, y_, t_] = (D[D[f[x, z, y, t], y], y] + 2/q[x]*D[D[f[x, z, y, t], y], z] + 
+     1/q[x]^2*D[D[f[x, z, y, t], z], z])/absb[x]^2;
+    *)
+
+
+(*
+Define normalised rho and MMS solution in terms of mode numbers \
+given above
+*)
+MmsDens[x_, z_, y_, t_] = amp*Sin[2.0*Pi*kx*xn[x]]*Sin[kz*z - phz]*Cos[ky*y- phy]*Sin[omega*t - pht];
+MmsUpar[x_, z_, y_, t_]=1;
+
+
+
+pflux[x_, z_, y_, t_]=MmsDens[x, z, y, t];
+(*Smms[x_, z_, y_, t_]=D[MmsDens[x,z,y,t],t]-d2dpar2[MmsDens,x,z,y,t]-Laplaceperpe[MmsDens,x,z,y,t];*)
+Smms[x_, z_, y_, t_]=D[MmsDens[x,z,y,t],t]-Dperp * LaplacePerp[MmsDens,x,z,y,t]-d2dpar2[MmsDens,x,z,y,t];
+
+
+Print["Finished MMS Terms"];
+
+(* Set a dummy return value *)
+1
+
+
+EndPackage[ ]
---
+++ MMS/distorted_grid/distorted_grid.cxx
@@ -0,0 +1,413 @@
+#include <bout/physicsmodel.hxx>
+
+#include <bout/constants.hxx>
+#include <bout/initialprofiles.hxx>
+#include <bout/interpolation.hxx>
+#include <bout/invert_laplace.hxx>
+#include <bout/invert_parderiv.hxx>
+#include <field_factory.hxx>
+#include <bout/derivs.hxx>
+#include <bout/assert.hxx>
+#include <bout/fv_ops.hxx>
+#include <cmath>
+using bout::globals::mesh;
+
+
+
+BoutReal BOUTMIN(const BoutReal &a, const BoutReal &b, const BoutReal &c,
+                 const BoutReal &d) {
+  BoutReal r1 = (a < b) ? a : b;
+  BoutReal r2 = (c < d) ? c : d;
+  return (r1 < r2) ? r1 : r2;
+}
+
+struct Stencil1D {
+  // Cell centre values
+  BoutReal c, m, p, mm, pp;
+
+  // Left and right cell face values
+  BoutReal L, R;
+};
+
+// First order upwind for testing
+void Upwind(Stencil1D &n) { n.L = n.R = n.c; }
+
+// Fromm method
+void Fromm(Stencil1D &n) {
+  n.L = n.c - 0.25 * (n.p - n.m);
+  n.R = n.c + 0.25 * (n.p - n.m);
+}
+
+/// The minmod function returns the value with the minimum magnitude
+/// If the inputs have different signs then returns zero
+BoutReal minmod(BoutReal a, BoutReal b) {
+  if (a * b <= 0.0)
+    return 0.0;
+
+  if (fabs(a) < fabs(b))
+    return a;
+  return b;
+}
+
+BoutReal minmod(BoutReal a, BoutReal b, BoutReal c) {
+  // If any of the signs are different, return zero gradient
+  if ((a * b <= 0.0) || (a * c <= 0.0)) {
+    return 0.0;
+  }
+
+  // Return the minimum absolute value
+  return SIGN(a) * BOUTMIN(fabs(a), fabs(b), fabs(c));
+}
+
+void MinMod(Stencil1D &n) {
+  // Choose the gradient within the cell
+  // as the minimum (smoothest) solution
+  BoutReal slope = minmod(n.p - n.c, n.c - n.m);
+  n.L = n.c - 0.5 * slope; // 0.25*(n.p - n.m);
+  n.R = n.c + 0.5 * slope; // 0.25*(n.p - n.m);
+}
+
+// Monotonized Central limiter (Van-Leer)
+void MC(Stencil1D &n) {
+  BoutReal slope =
+      minmod(2. * (n.p - n.c), 0.5 * (n.p - n.m), 2. * (n.c - n.m));
+  n.L = n.c - 0.5 * slope;
+  n.R = n.c + 0.5 * slope;
+}
+
+
+
+
+
+// Square function for vectors
+Field3D SQ(const Vector3D &v) { return v * v; }
+
+void setRegions(Field3D &f) {
+  f.yup().setRegion("RGN_YPAR_+1");
+  f.ydown().setRegion("RGN_YPAR_-1");
+}
+
+const Field3D &yup(const Field3D &f) { return f.yup(); }
+BoutReal yup(BoutReal f) { return f; };
+const Field3D &ydown(const Field3D &f) { return f.ydown(); }
+BoutReal ydown(BoutReal f) { return f; };
+const BoutReal yup(BoutReal f, Ind3D i) { return f; };
+const BoutReal ydown(BoutReal f, Ind3D i) { return f; };
+// const BoutReal& yup(const Field3D &f, Ind3D i) { return f.yup()[i.yp()]; }
+// const BoutReal& ydown(const Field3D &f, Ind3D i) { return f.ydown()[i.ym()];
+// } BoutReal& yup(Field3D &f, Ind3D i) { return f.yup()[i.yp()]; } BoutReal&
+// ydown(Field3D &f, Ind3D i) { return f.ydown()[i.ym()]; }
+const BoutReal &yup(const Field3D &f, Ind3D i) { return f.yup()[i]; }
+const BoutReal &ydown(const Field3D &f, Ind3D i) { return f.ydown()[i]; }
+BoutReal &yup(Field3D &f, Ind3D i) { return f.yup()[i]; }
+BoutReal &ydown(Field3D &f, Ind3D i) { return f.ydown()[i]; }
+const BoutReal &_get(const Field3D &f, Ind3D i) { return f[i]; }
+BoutReal &_get(Field3D &f, Ind3D i) { return f[i]; }
+BoutReal _get(BoutReal f, Ind3D i) { return f; };
+BoutReal copy(BoutReal f) { return f; };
+
+
+
+
+
+
+void alloc_all(Field3D &f) {
+  f.allocate();
+  f.splitParallelSlices();
+  f.yup().allocate();
+  f.ydown().allocate();
+  setRegions(f);
+}
+
+#define GET_ALL(name)                                                          \
+  auto *name##a = &name[Ind3D(0)];                                             \
+  auto *name##b = &name.yup()[Ind3D(0)];                                       \
+  auto *name##c = &name.ydown()[Ind3D(0)];
+
+#define DO_ALL(op, name)                                                       \
+  template <class A, class B> Field3D name##_all(const A &a, const B &b) {     \
+    Field3D result;                                                            \
+    alloc_all(result);                                                         \
+    BOUT_FOR(i, result.getRegion("RGN_ALL")) { name##_all(result, a, b, i); }  \
+    setRegions(result);                                                        \
+    return result;                                                             \
+  }                                                                            \
+  template <class A, class B>                                                  \
+  void name##_all(Field3D &result, const A &a, const B &b, Ind3D i) {          \
+    result[i] = op(_get(a, i), _get(b, i));                                    \
+    yup(result, i) = op(yup(a, i), yup(b, i));                                 \
+    ydown(result, i) = op(ydown(a, i), ydown(b, i));                           \
+  }                                                                            \
+  template <class B> void name##_all(Field3D &result, const B &b, Ind3D i) {   \
+    result[i] = op(result[i], _get(b, i));                                     \
+    yup(result, i) = op(yup(result, i), yup(b, i));                            \
+    ydown(result, i) = op(ydown(result, i), ydown(b, i));                      \
+  }
+
+DO_ALL(floor, floor)
+DO_ALL(pow, pow)
+
+#undef DO_ALL
+#define DO_ALL(op, name)                                                       \
+  template <class A, class B> Field3D name##_all(const A &a, const B &b) {     \
+    Field3D result;                                                            \
+    alloc_all(result);                                                         \
+    BOUT_FOR(i, result.getRegion("RGN_ALL")) { name##_all(result, a, b, i); }  \
+    checkData(result, "RGN_ALL");                                              \
+    setRegions(result);                                                        \
+    return result;                                                             \
+  }                                                                            \
+  Field3D name##_all(const Field3D &a, const Field3D &b) {                     \
+    Field3D result;                                                            \
+    alloc_all(result);                                                         \
+    const int n = result.getNx() * result.getNy() * result.getNz();            \
+    GET_ALL(result);                                                           \
+    GET_ALL(a);                                                                \
+    GET_ALL(b);                                                                \
+    BOUT_OMP(omp parallel for simd)                                            \
+    for (int i = 0; i < n; ++i) {                                              \
+      resulta[i] = aa[i] op ba[i];                                             \
+      resultb[i] = ab[i] op bb[i];                                             \
+      resultc[i] = ac[i] op bc[i];                                             \
+    }                                                                          \
+    setRegions(result);                                                        \
+    return result;                                                             \
+  }                                                                            \
+  Field3D name##_all(const Field3D &a, BoutReal b) {                           \
+    Field3D result;                                                            \
+    alloc_all(result);                                                         \
+    const int n = result.getNx() * result.getNy() * result.getNz();            \
+    GET_ALL(result);                                                           \
+    GET_ALL(a);                                                                \
+    BOUT_OMP(omp parallel for simd)                                            \
+    for (int i = 0; i < n; ++i) {                                              \
+      resulta[i] = aa[i] op b;                                                 \
+      resultb[i] = ab[i] op b;                                                 \
+      resultc[i] = ac[i] op b;                                                 \
+    }                                                                          \
+    setRegions(result);                                                        \
+    return result;                                                             \
+  }                                                                            \
+  template <class A, class B>                                                  \
+  void name##_all(Field3D &result, const A &a, const B &b, Ind3D i) {          \
+    result[i] = _get(a, i) op _get(b, i);                                      \
+    yup(result, i) = yup(a, i) op yup(b, i);                                   \
+    ydown(result, i) = ydown(a, i) op ydown(b, i);                             \
+  }
+
+// void div_all(Field3D & result, const Field3D & a, const Field3D & b, Ind3D i)
+// {
+//   result[i] = a[i] / b[i];
+//   result.yup()[i.yp()] = a.yup()[i.yp()] / b.yup()[i.yp()];
+//   result.ydown()[i.ym()] = a.ydown()[i.ym()] / b.ydown()[i.ym()];
+// }
+
+//#include "mul_all.cxx"
+DO_ALL(*, mul)
+DO_ALL(/, div)
+DO_ALL(+, add)
+DO_ALL(-, sub)
+#undef DO_ALL
+
+#define DO_ALL(op, name)                                                       \
+  template <class A, class B> Field3D &name##_all_inp(A &a, const B &b) {      \
+    BOUT_FOR(i, a.getRegion("RGN_ALL")) { name##_all(a, b, i); }               \
+    checkData(a, "RGN_ALL");                                                   \
+    return a;                                                                  \
+  }                                                                            \
+  Field3D &name##_all_inp(Field3D &a, const Field3D &b) {                      \
+    const int n = a.getNx() * a.getNy() * a.getNz();                           \
+    GET_ALL(a);                                                                \
+    GET_ALL(b);                                                                \
+    BOUT_OMP(omp parallel for simd)                                            \
+    for (int i = 0; i < n; ++i) {                                              \
+      aa[i] op ba[i];                                                          \
+      ab[i] op bb[i];                                                          \
+      ac[i] op bc[i];                                                          \
+    }                                                                          \
+    return a;                                                                  \
+  }                                                                            \
+  Field3D &name##_all_inp(Field3D &a, BoutReal b) {                            \
+    const int n = a.getNx() * a.getNy() * a.getNz();                           \
+    GET_ALL(a);                                                                \
+    BOUT_OMP(omp parallel for simd)                                            \
+    for (int i = 0; i < n; ++i) {                                              \
+      aa[i] op b;                                                              \
+      ab[i] op b;                                                              \
+      ac[i] op b;                                                              \
+    }                                                                          \
+    return a;                                                                  \
+  }                                                                            \
+  template <class A, class B> void name##_all_inp(A &a, const B &b, Ind3D i) { \
+    a[i] op _get(b, i);                                                        \
+    yup(a, i) op yup(b, i);                                                    \
+    ydown(a, i) op ydown(b, i);                                                \
+  }
+
+// #include "mul_all.cxx"
+DO_ALL(*=, mul)
+DO_ALL(/=, div)
+DO_ALL(+=, add)
+DO_ALL(-=, sub)
+
+#undef DO_ALL
+#define DO_ALL(op)                                                             \
+  inline void op##_all(Field3D &result, const Field3D &a, Ind3D i) {           \
+    result[i] = op(a[i]);                                                      \
+    yup(result, i) = op(yup(a, i));                                            \
+    ydown(result, i) = op(ydown(a, i));                                        \
+  }                                                                            \
+  inline Field3D op##_all(const Field3D &a) {                                  \
+    Field3D result;                                                            \
+    alloc_all(result);                                                         \
+    BOUT_FOR(i, result.getRegion("RGN_ALL")) { op##_all(result, a, i); }       \
+    checkData(result, "RGN_ALL");                                              \
+    setRegions(result);                                                        \
+    return result;                                                             \
+  }
+
+DO_ALL(sqrt)
+DO_ALL(SQ)
+DO_ALL(copy)
+DO_ALL(exp)
+DO_ALL(log)
+
+#undef DO_ALL
+
+
+const Field3D new_Delp2(const Field3D& a){
+  auto *coord = mesh->getCoordinates();
+  Field3D tmp = (DDX(coord->J * coord->g11)*DDX(a) + coord->J * coord->g11 * D2DX2(a))/coord->J;
+  tmp += (DDZ(coord->J * coord->g33)*DDZ(a) + coord->J * coord->g33 * D2DZ2(a))/coord->J;
+  tmp += (DDX(coord->J * coord->g13)*DDZ(a) + coord->J * coord->g13 * D2DXDZ(a) *2.0 + DDZ(coord->J * coord->g13)*DDX(a))/coord->J;
+  return tmp;
+}
+
+const Field3D power(const Field3D& a, const int powval){
+  return pow(a,powval);
+}
+
+
+class distorted_grid : public PhysicsModel {
+private:
+    Field3D Ne;	
+  Field3D theta,rho_n,rho;
+  BoutReal diffusion,dpar;
+  Field3D Ne_source,Ne_solution,xl,yl,zl;
+  Field3D debug_diffusion, debug_pardiffusion;
+protected:
+  int init(bool UNUSED(restart)) override {
+
+    TRACE("LOAD DATA AND OPTIONS");
+    auto& opt = Options::root();
+    auto *coord = mesh->getCoordinates();
+
+    auto& optne = Options::root()["Ne"];
+
+    OPTION(opt,diffusion,0.0);
+    OPTION(opt,dpar,0.0);
+    xl = opt["xl"].withDefault(Field3D{0.0});
+    yl = opt["yl"].withDefault(Field3D{0.0});
+    zl = opt["zl"].withDefault(Field3D{0.0});
+
+    SAVE_ONCE(xl,yl,zl);
+    
+    Ne=0.0;
+    Ne_source = 0.0;
+    Ne_solution = 0.0;
+    SAVE_REPEAT(Ne_source,Ne_solution);
+    
+    SOLVE_FOR(Ne);
+    theta=0.0;
+    mesh->get(theta,"theta");
+    SAVE_ONCE(theta);
+
+    rho_n=0.0;
+    mesh->get(rho_n,"rho_n");
+    SAVE_ONCE(rho_n);
+
+    rho=0.0;
+    mesh->get(rho,"rho");
+    SAVE_ONCE(rho);
+
+    xl=rho;
+    zl=theta;
+
+    debug_diffusion = 0.0;
+    debug_pardiffusion = 0.0;
+    SAVE_REPEAT(debug_diffusion,debug_pardiffusion);
+    
+    return 0;
+  }
+  
+  int rhs(BoutReal t) override {
+    //2*cos(0.5 - yl)*sin(0. - 0.1*t)*sin(15.70796326794897*(-0.4 + xl))*sin(0.1 - 4*zl)
+    //Ne_solution = 2*cos(0.5 - yl)*sin(0. - 0.1*t)*sin(31.41592653589794*(-0.4 + xl))*sin(0.1 - 4*zl);
+    Ne_solution = 0.4*cos(0.5 - 1.*yl)*sin(0. - 0.1*t)*sin(15.70796326794897*(-0.4 + xl))*sin(0.1 - 4*zl);
+    if ((diffusion <= 0.0) && (dpar <= 0.0)){
+      
+      Ne_source = -0.4*cos(0.5 - 1.*yl)*cos(0. - 0.1*t)*sin(15.70796326794897*(-0.4 + xl))*sin(0.1 - 4*zl)/10.0;
+      
+    } else if ((diffusion) == 0.001 && (dpar == 1.0)) {
+      
+      Ne_source = -0.04000000000000001*cos(0. - 0.1*t)*cos(0.5 - 1.*yl)*sin(15.70796326794897*(-0.4 + xl))*sin(0.1 - 4*zl) - 0.001*((6.283185307179587*cos(15.70796326794897*(-0.4 + xl))*cos(0.5 - 1.*yl)*sin(0. - 0.1*t)*sin(0.1 - 4*zl))/xl - 98.69604401089363*cos(0.5 - 1.*yl)*sin(0. - 0.1*t)*sin(15.70796326794897*(-0.4 + xl))*sin(0.1 - 4*zl) - (6.4*cos(0.5 - 1.*yl)*sin(0. - 0.1*t)*sin(15.70796326794897*(-0.4 + xl))*sin(0.1 - 4*zl))/power(xl,2)) - (1.*((-3.2*cos(0.1 - 4*zl)*sin(0. - 0.1*t)*sin(15.70796326794897*(-0.4 + xl))*sin(0.5 - 1.*yl))/(8. - 0.2*power(xl,2)) - 0.4*cos(0.5 - 1.*yl)*sin(0. - 0.1*t)*sin(15.70796326794897*(-0.4 + xl))*sin(0.1 - 4*zl) - (6.4*cos(0.5 - 1.*yl)*sin(0. - 0.1*t)*sin(15.70796326794897*(-0.4 + xl))*sin(0.1 - 4*zl))/power(8. - 0.2*power(xl,2),2)))/(1 + power(xl,2)/power(8. - 0.2*power(xl,2),2));
+    } else {
+      
+      BoutException("No source");
+      
+    }
+    
+    Ne.applyBoundary();
+      
+    if (mesh->firstX()) {
+      for (int j = mesh->ystart; j <= mesh->yend; j++) {
+	for (int k = 0; k < mesh->LocalNz; k++) {
+	  BoutReal bndryval = 0.5*(Ne_solution(1,j,k)+Ne(2,j,k));
+	  Ne(1,j,k) = 2.0*bndryval - Ne(2,j,k);
+	  
+	  //Ne(0,j,k) = Ne_solution(0,j,k);
+	  //Ne(1,j,k) =	Ne_solution(1,j,k);
+	}
+      }
+    }
+
+      
+    if (mesh->lastX()) {
+      int n = mesh->LocalNx;
+      for (int j = mesh->ystart; j <= mesh->yend; j++) {
+	  for (int k = 0; k < mesh->LocalNz; k++) {
+	    BoutReal bndryval = 0.5*(Ne_solution(n-2,j,k)+Ne(n-3,j,k));
+	    
+	    Ne(n-2,j,k) = 2.0*bndryval - Ne(n-3,j,k);
+	    
+	    //Ne(n-1,j,k) = Ne_solution(n-1,j,k);
+	    //Ne(n-2,j,k) = Ne_solution(n-2,j,k)
+	  }
+      }
+    }
+    
+      
+
+      
+    mesh->communicate(Ne);
+    Ne.applyParallelBoundary();
+    ddt(Ne) = 0.0;
+    ddt(Ne) += Ne_source;
+    
+    if (diffusion>0.0){
+      debug_diffusion = diffusion*new_Delp2(Ne);
+      ddt(Ne) += debug_diffusion;
+    }
+
+    if (dpar > 0.0){
+      debug_pardiffusion = dpar * Grad2_par2(Ne);
+      ddt(Ne) += debug_pardiffusion;
+    }
+      
+    return 0;
+  }
+
+
+};
+  
+BOUTMAIN(distorted_grid);
