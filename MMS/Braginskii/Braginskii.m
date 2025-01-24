4e2d30e6766868cb0718501c64f952cebab6bcab
totork <ttork96@gmail.com>
2025-01-24 17:52:01 +0100

    Braginskii MMS skeleton complete, now all tersm ahve to be added sequentially

--- MMS/Braginskii/Braginskii.m
+++ MMS/Braginskii/Braginskii.m
@@ -1,6 +1,8 @@
 (* ::Package:: *)
 
 (* ::Package:: *)
+(**)
+
 
 BeginPackage["BraginskiiMMSSlab`"]
 
@@ -23,11 +25,37 @@ define operators and normalised coordinates
 *)
 curvature[f_, x_, y_, z_, t_] = -2*D[f[x,y,z,t],y];
 laplaceperp[f_, x_, y_, z_, t_] = D[f[x,y,z,t],{x,2}] + D[f[x,y,z,t],{z,2}];
+hyperdiffusion[f_, x_, y_, z_, t_] = D[f[x,y,z,t],{x,4}] + D[f[x,y,z,t],{z,4}];
 arakawa[u_, v_, r_, p_, z_, t_] = 
   D[u[x, y, z, t],x]*D[v[x, y, z, t],z] - 
    D[u[x,y,z,t],z]*D[v[x,y,z,t],x];
 xn[x_] =(x-xmin)/(xmax-xmin); 
-zn[z_]=(z-zmin)/(zmax-zmin); 
+zn[z_]=(z-zmin)/(zmax-zmin);
+ 
+
+
+(*
+Quantities for the normalization of the code
+
+*)
+Tnorm = 20;
+Nnorm = 10^19;
+Bnorm = 1.0;
+AA = 2.0;
+Mp=1.672621*(10^-27);
+Me=9.1093*(10^-31);
+qe=1.602176*(10^-19);
+Cs0 = Sqrt[(qe*Tnorm)/(AA*Mp)];
+Omegaci = qe*Bnorm/(AA*Mp);
+rhos0=Cs0/Omegaci;
+mime = AA * Mp / (Me);
+memi = Me/(AA*Mp);
+
+
+(*
+Switches and quantities for the density time evolution
+*)
+
 
 
 SolNe[x_, y_, z_, t_] = OffsetNe + ampNe*Sin[2.0*Pi*kxNe*xn[x]]*Sin[kyNe*y - phyNe]*Sin[2.0*Pi*kzNe*zn[z]-phzNe]*Sin[2.0*Pi*omegaNe*t - phtNe];
@@ -42,7 +70,9 @@ SolTi[x_, y_, z_, t_] = SolPi[x,y,z,t]/SolNe[x,y,z,t];
 SolVi[x_, y_, z_, t_] = SolNVi[x,y,z,t]/SolNe[x,y,z,t];
 SolVe[x_, y_, z_, t_] = SolVePsi[x,y,z,t]+SolVi[x,y,z,t];
 
-SourceNe[x_, y_, z_, t_] = D[SolNe[x,y,z,t],t];
+SourceNe[x_, y_, z_, t_] = D[SolNe[x,y,z,t],t]\
+	-rhos0*rhos0*SWNeanomalous * Danomalous*laplaceperp[SolNe,x,y,z,t]/(rhos0*rhos0*Omegaci)\
+	+SWNehyper * (rhos0^4)*(hyperD/(rhos0^4 * Omegaci)) * hyperdiffusion[SolNe,x,y,z,t];
 SourceNVi[x_, y_, z_, t_] = D[SolNVi[x,y,z,t],t];
 SourcePe[x_, y_, z_, t_] = D[SolPe[x,y,z,t],t];
 SourcePi[x_, y_, z_, t_] = D[SolPi[x,y,z,t],t];
