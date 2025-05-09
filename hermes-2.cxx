0590fd57d0331de1692d834e22d59701a7edd8f7
totork <ttork96@gmail.com>
2025-05-09 20:09:19 +0200

    fixed bug

--- hermes-2.cxx
+++ hermes-2.cxx
@@ -3963,7 +3963,7 @@ int Hermes::rhs(BoutReal t) {
       } else {
         tmp = Div_par_K_Grad_par_mod(div_all(mul_all(Pi,tau_i),coord->Bxy),mul_all(B12,Vi), true);
       }
-      Pi_parviscousheat = -Vi * 1.28*B12*tmp;
+      TE_Pi_parviscousheat = -Vi * 1.28*B12*tmp;
       ddt(Pi) += Pi_parviscousheat;      
       
     }
