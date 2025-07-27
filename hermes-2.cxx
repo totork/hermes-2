3f72cd2df48d0d2812e3f8ac97939f8bed9056a6
totork <ttork96@gmail.com>
2025-07-28 01:48:54 +0200

    Added vort parflow

--- hermes-2.cxx
+++ hermes-2.cxx
@@ -4436,7 +4436,18 @@ int Hermes::rhs(BoutReal t) {
 	}
       }// End Vort_polarcurrent
           
-
+      if (Vort_parflow){
+	TRACE("Vorticity parallel flow");
+	if (!use_new_div_par){
+	  Field3D VortVi = mul_all(Vort,Vi);
+	  TE_Vort_parflow = -Div_par(VortVi);
+	} else if (use_H3_div_par){
+	  TE_Vort_parflow = -Div_par_mod_H3(Vort,Vi,fastest_ispeed);
+	} else {
+	  TE_Vort_parflow = -Div_par_mod(Vort,Vi,fastest_ispeed, use_slope_limiter);
+	}
+	ddt(Vort) += TE_Vort_parflow;
+      }
 
       
       
